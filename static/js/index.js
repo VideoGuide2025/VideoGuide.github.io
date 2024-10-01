5c7c9e49e8d323604dfaf4bfca5b9fad904df9b6
ACDC2025 <acdc2025lg@gmail.com>
2024-10-01 17:17:22 +0900

    test

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,13 @@ window.HELP_IMPROVE_VIDEOJS = false;
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
+	$(".twentytwenty-container").twentytwenty({
+        default_offset_pct: 0.5, // Sets the default split between before and after
+        orientation: 'horizontal', // Makes sure the split is horizontal
+        before_label: 'Before', // Sets the before label
+        after_label: 'After' // Sets the after label
+    });
+
     var options = {
 			slidesToScroll: 1,
 			slidesToShow: 1,
@@ -16,14 +23,6 @@ $(document).ready(function() {
 		// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
 
-	
-	$(".twentytwenty-container").twentytwenty({
-        default_offset_pct: 0.5, // Sets the default split between before and after
-        orientation: 'horizontal', // Makes sure the split is horizontal
-        before_label: 'Before', // Sets the before label
-        after_label: 'After' // Sets the after label
-    });
-	
     bulmaSlider.attach();
 
 })
